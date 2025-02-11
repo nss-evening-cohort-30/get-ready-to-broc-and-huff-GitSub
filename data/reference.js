f2014131a67d4b4d86a7b5da9b8e77d5a7d58293
christophuff <46765167+christophuff@users.noreply.github.com>
2025-02-10 21:13:51 -0600

    data folder updated

---
+++ data/reference.js
@@ -0,0 +1,125 @@
+const projects = [
+    {
+        project_id: 1,
+        title: "",
+        description: "", 
+        pinned: true,
+        favorite: false,
+        public: true
+    },
+
+    {
+        project_id: 2,
+        title: "",
+        description: "", 
+        pinned: true,
+        favorite: false,
+        public: true
+    },
+    
+    {
+        project_id: 3,
+        title: "",
+        description: "", 
+        pinned: true,
+        favorite: false,
+        public: true
+    },
+    
+    {
+        project_id: 4,
+        title: "",
+        description: "", 
+        pinned: true,
+        favorite: false,
+        public: true
+    },
+
+    {
+        project_id: 5,
+        title: "",
+        description: "", 
+        pinned: true,
+        favorite: false,
+        public: true
+    },
+]
+
+const repositories = [
+    {
+        repo_id: 1,
+        title: "",
+        description: "",
+        open: true,
+        public: true
+    }
+
+    {
+        repo_id: 2,
+        title: "",
+        description: "",
+        open: true,
+        public: true
+    }
+
+    {
+        repo_id: 3,
+        title: "",
+        description: "",
+        open: true,
+        public: true
+    }
+
+    {
+        repo_id: 4,
+        title: "",
+        description: "",
+        open: true,
+        public: true
+    }
+
+    {
+        repo_id: 5,
+        title: "",
+        description: "",
+        open: true,
+        public: true
+    }
+]
+
+const packages = [
+    {
+        packages_id: 1,
+        title: "",
+        description: "",
+        public: true
+    },
+
+    {
+        packages_id: 2,
+        title: "",
+        description: "",
+        public: true
+    },
+
+    {
+        packages_id: 3,
+        title: "",
+        description: "",
+        public: true
+    },
+
+    {
+        packages_id: 4,
+        title: "",
+        description: "",
+        public: true
+    },
+
+    {
+        packages_id: 5,
+        title: "",
+        description: "",
+        public: true
+    }
+]

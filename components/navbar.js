118009df32bea02dae26d5bcdb1069fc78811c60
Chardonne Dixon <chardonne.dixon@gmail.com>
2025-02-11 19:30:58 -0600

    Update navbar.js
    rough draft to navBar {for team review}

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,32 @@
-export const navBar =
-     `
-    <nav class="navbar navbar-expand-lg">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-    </nav>
-  `;
+ function createHeader() {
+            const domString = `
+                <nav class="navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            `;
+            
+            const header = document.createElement('header');
+            header.style.backgroundColor = '#C4C4C4';
+            header.style.width = '100%';
+            header.style.height = '96px';
+            header.style.textAlign = 'left';
+            header.style.padding = '20px';
+            header.style.boxSizing = 'border-box';
+            
+            header.innerHTML = domString;
+            
+            document.body.insertBefore(header, document.body.firstChild);
+        }
